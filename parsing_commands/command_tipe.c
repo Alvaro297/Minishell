521661cbe05b0848438a83ed1f03b7b3f83ade3d
Alvaro297 <alvaromartinfalagan29@gmail.com>
2025-01-23 19:03:21 +0100

    Comandos internos

--- parsing_commands/command_tipe.c
+++ parsing_commands/command_tipe.c
@@ -21,13 +21,17 @@ static void	internal_commands(char *input, char **envp)
 
 void	command_tipe(char *input, char **envp)
 {
+	//Estos son los comandos internos todavia hasy muchos mas tipos de comandos que son:
+	//Comandos externos: ls, grep, cat, gcc
+	//Redirecciones: < > >> <<
+	//Pipes: |
+	//Comandos en Segundo plano: &
+	//Todo este tipo de comandos irán aquí
 	if (ft_strncmp(input, "exit ", 5) == 0 || ft_strncmp(input, "cd ", 3) == 0 ||
 		ft_strncmp(input, "pwd ", 4) == 0 || ft_strncmp(input, "export ", 8) == 0 ||
 		ft_strncmp(input, "unset ", 6) == 0 || ft_strncmp(input, "env ", 4) == 0 ||
 		ft_strncmp(input, "echo ", 5) == 0)
-	{
 		internal_commands(input, envp);
-	}
 	
 	
 }
