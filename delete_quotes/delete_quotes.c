78eeaaac7fba9d939681fc6db2bab2492f3ec5a3
Pablo Perez Merchan <paperez-@c2r17s6.42madrid.com>
2025-05-21 19:47:14 +0200

    retocatr heredocs

--- delete_quotes/delete_quotes.c
+++ delete_quotes/delete_quotes.c
@@ -98,6 +98,4 @@ void	delete_quotes(t_minishell *minishell, t_cmd *cmd)
 		cmd->args = delete_quotes_double_array(minishell, cmd->args, true);
 	if (cmd->outfile_array)
 		cmd->outfile_array = delete_quotes_double_array(minishell, cmd->outfile_array, true);
-	if (cmd->here_doc_delim && cmd->is_heredoc)
-		cmd->here_doc_delim = delete_quotes_double_array(minishell, cmd->here_doc_delim, false);
 }
